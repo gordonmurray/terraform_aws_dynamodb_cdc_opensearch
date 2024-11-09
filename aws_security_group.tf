43148e0c2b65f7deece8295a70ffff8b537a72be
Gordon Murray <gordon@gordonmurray.com>
2024-11-09 23:03:29 +0000

    update sg

--- aws_security_group.tf
+++ aws_security_group.tf
@@ -1,12 +1,13 @@
 
-resource "aws_security_group" "example" {
+resource "aws_security_group" "opensearch" {
+  name   = "opensearch"
   vpc_id = var.vpc_id
 
   ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Adjust for your security requirements
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
