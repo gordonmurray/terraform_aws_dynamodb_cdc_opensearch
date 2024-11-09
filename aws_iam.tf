232dde46b37a5affebb1b6a728dd1f922c64a0cc
Gordon Murray <gordon@gordonmurray.com>
2024-11-09 23:03:12 +0000

    generous permissions

--- aws_iam.tf
+++ aws_iam.tf
@@ -8,7 +8,10 @@ resource "aws_iam_role" "opensearch_access_role" {
         Action = "sts:AssumeRole"
         Effect = "Allow"
         Principal = {
-          Service = "opensearchservice.amazonaws.com"
+          Service = [
+            "opensearchservice.amazonaws.com",
+            "osis-pipelines.amazonaws.com"
+          ]
         }
       }
     ]
@@ -23,23 +26,86 @@ resource "aws_iam_policy" "ddb_to_opensearch_policy" {
     Statement = [
       {
         Action = [
-          "dynamodb:GetRecords",
+          "es:*",
+          "opensearch:*",
+          "s3:PutObject",
+          "s3:GetObject",
           "dynamodb:GetShardIterator",
+          "es:ESHttpPost",
+          "es:ESHttpGet",
+          "es:DescribeDomain",
           "dynamodb:DescribeStream",
-          "dynamodb:ListStreams"
+          "s3:ListBucket",
+          "es:ESHttpPut",
+          "dynamodb:GetRecords",
         ]
         Effect = "Allow"
         Resource = [
           aws_dynamodb_table.users_table.stream_arn,
-          aws_dynamodb_table.products_table.stream_arn
+          aws_dynamodb_table.products_table.stream_arn,
+          "${aws_opensearch_domain.example_domain.arn}/*",
+          aws_s3_bucket.backups.arn,
+          "${aws_s3_bucket.backups.arn}/*",
         ]
       },
       {
         Action = [
-          "es:ESHttpPost"
+          "es:DeleteOutboundConnection",
+          "es:UpdateVpcEndpoint",
+          "es:DeletePackage",
+          "es:ListVpcEndpoints",
+          "es:ListVpcEndpointsForDomain",
+          "es:ListElasticsearchInstanceTypeDetails",
+          "es:ListDomainsForPackage",
+          "es:ListInstanceTypeDetails",
+          "es:AuthorizeVpcEndpointAccess",
+          "es:AcceptInboundConnection",
+          "es:DeleteElasticsearchServiceRole",
+          "es:DescribeInboundConnections",
+          "es:DescribeOutboundConnections",
+          "es:DescribeReservedInstances",
+          "es:AcceptInboundCrossClusterSearchConnection",
+          "es:DescribeReservedInstanceOfferings",
+          "es:DescribeInstanceTypeLimits",
+          "es:DescribeVpcEndpoints",
+          "es:ListVpcEndpointAccess",
+          "es:DeleteInboundCrossClusterSearchConnection",
+          "es:DescribeOutboundCrossClusterSearchConnections",
+          "es:DeleteOutboundCrossClusterSearchConnection",
+          "es:DescribeReservedElasticsearchInstanceOfferings",
+          "es:CreateServiceRole",
+          "es:CreateElasticsearchServiceRole",
+          "es:UpdatePackage",
+          "es:RejectInboundCrossClusterSearchConnection",
+          "es:DeleteInboundConnection",
+          "es:GetPackageVersionHistory",
+          "es:RejectInboundConnection",
+          "es:PurchaseReservedElasticsearchInstanceOffering",
+          "es:CreateVpcEndpoint",
+          "es:DescribeInboundCrossClusterSearchConnections",
+          "es:ListVersions",
+          "es:DescribeReservedElasticsearchInstances",
+          "es:ListDomainNames",
+          "es:PurchaseReservedInstanceOffering",
+          "es:CreatePackage",
+          "es:DeleteVpcEndpoint",
+          "es:DescribePackages",
+          "es:ListElasticsearchInstanceTypes",
+          "es:ListElasticsearchVersions",
+          "es:DescribeElasticsearchInstanceTypeLimits",
+          "es:CreateApplication",
+          "es:RevokeVpcEndpointAccess"
         ]
         Effect   = "Allow"
-        Resource = aws_opensearch_domain.example_domain.arn
+        Resource = "*"
+      },
+      {
+        Action = "dynamodb:ListStreams"
+        Effect = "Allow"
+        Resource = [
+          "${aws_dynamodb_table.users_table.arn}/stream/*",
+          "${aws_dynamodb_table.products_table.arn}/stream/*"
+        ]
       }
     ]
   })
