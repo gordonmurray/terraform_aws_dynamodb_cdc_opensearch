4b4512c4d2676f30945c980fa3a46a6c805cb42c
Gordon Murray <gordon@gordonmurray.com>
2024-11-09 23:03:03 +0000

    cloudwatch log

--- aws_cloudwatch.tf
+++ aws_cloudwatch.tf
@@ -1,10 +1,16 @@
 
 resource "aws_cloudwatch_log_group" "opensearch_logs" {
   name              = "/aws/opensearch/opensearch-domain-logs"
-  retention_in_days = 14
+  retention_in_days = 5
   kms_key_id        = aws_kms_key.cloudwatch_logs.arn
 }
 
+
+resource "aws_cloudwatch_log_group" "application_logs" {
+  name              = "/aws/OpenSearchService/domains/example-domain/application-logs"
+  retention_in_days = 5
+  kms_key_id        = aws_kms_key.cloudwatch_logs.arn
+}
 
 resource "aws_cloudwatch_log_resource_policy" "opensearch_log_policy" {
   policy_name = "OpenSearchLogPolicy"
